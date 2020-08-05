86367e0afb7aa92a58a58d6474b395d6a811c4aa
loan-petit <petit.loan1@gmail.com>
2020-08-05 21:32:41 +0200

    feature: List appointments on customers modal in order for the service provider to easily manage appointments of customers.

--- next-app/src/components/adminSite/AppointmentModal.tsx
+++ next-app/src/components/adminSite/AppointmentModal.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 import Appointment from '../../models/Appointment'
 import Router from 'next/router'
@@ -8,64 +8,83 @@ import Router from 'next/router'
 type Props = {
   appointment?: Appointment
   onClose: () => void
+  useBackButton?: boolean
 }
 
 const AppointmentModal: React.FunctionComponent<Props> = ({
   appointment,
   onClose,
+  useBackButton = false,
 }) => {
   if (!appointment) return <div />
 
   return (
     <>
       {/* Background greyed out */}
-      <div className='fixed inset-0 bg-gray-700 opacity-25' />
+      <div className="fixed inset-0 bg-gray-700 opacity-25" />
 
       {/* Modal */}
-      <div className='fixed inset-0 z-50 flex items-center justify-center'>
-        <div className='my-3 bg-white rounded-lg shadow-lg'>
-          <div className='flex items-center justify-between px-5 py-4 border-b border-gray-200'>
-            <h4 className='text-xl font-semibold'>
+      <div className="fixed inset-0 z-50 flex items-center justify-center">
+        <div className="my-3 bg-white rounded-lg shadow-lg">
+          <div
+            className={
+              'flex items-center px-5 py-4 border-b border-gray-200 ' +
+              (useBackButton ? '' : 'justify-between')
+            }
+          >
+            {useBackButton && (
+              <div>
+                <button onClick={onClose}>
+                  <FontAwesomeIcon
+                    icon={faArrowLeft}
+                    className="mr-6 text-gray-800"
+                  />
+                </button>
+              </div>
+            )}
+            <h4 className="text-xl font-semibold">
               {appointment.appointmentType?.name}
             </h4>
-            <div>
-              <button onClick={onClose}>
-                <FontAwesomeIcon
-                  icon={faTimesCircle}
-                  className='text-red-500 hover:text-red-600'
-                />
-              </button>
-            </div>
+            {!useBackButton && (
+              <div>
+                <button onClick={onClose}>
+                  <FontAwesomeIcon
+                    icon={faTimesCircle}
+                    className="text-red-500 hover:text-red-600"
+                  />
+                </button>
+              </div>
+            )}
           </div>
 
-          <div className='flex flex-col p-5'>
-            <label className='text-sm'>Description</label>
-            <p className='mt-1 text-gray-600'>
+          <div className="flex flex-col p-5">
+            <label className="text-sm">Description</label>
+            <p className="mt-1 text-gray-600">
               {appointment.appointmentType?.description}
             </p>
             <br />
 
-            <label className='text-sm'>Durée</label>
-            <p className='mt-1 text-gray-600'>
+            <label className="text-sm">Durée</label>
+            <p className="mt-1 text-gray-600">
               {appointment.appointmentType?.duration} minutes
             </p>
             <br />
 
-            <label className='text-sm'>Prix</label>
-            <p className='mt-1 text-gray-600'>
+            <label className="text-sm">Prix</label>
+            <p className="mt-1 text-gray-600">
               {appointment.appointmentType?.price} €
             </p>
             <br />
 
-            <label className='text-sm'>Nom du client</label>
-            <p className='mt-1 text-gray-600'>
+            <label className="text-sm">Nom du client</label>
+            <p className="mt-1 text-gray-600">
               {appointment.customer?.firstName} {appointment.customer?.lastName}
             </p>
           </div>
 
-          <div className='flex justify-end p-4'>
+          <div className="flex justify-end p-4">
             <button
-              className='px-3 py-2 mr-1 text-sm text-white bg-red-500 rounded hover:bg-red-600'
+              className="px-3 py-2 mr-1 text-sm text-white bg-red-500 rounded hover:bg-red-600"
               onClick={() =>
                 Router.push(`/appointments/cancelOne?id=${appointment.id}`)
               }
@@ -73,7 +92,7 @@ const AppointmentModal: React.FunctionComponent<Props> = ({
               Annuler le rendez-vous
             </button>
             <button
-              className='px-3 py-2 text-sm font-semibold text-gray-800 transition duration-150 hover:text-gray-900'
+              className="px-3 py-2 text-sm font-semibold text-gray-800 transition duration-150 hover:text-gray-900"
               onClick={() =>
                 Router.push(`/customers?id=${appointment.customer?.id}`)
               }
--- next-app/src/components/adminSite/CustomerModal.tsx
+++ next-app/src/components/adminSite/CustomerModal.tsx
@@ -6,6 +6,7 @@ import {
   faEnvelope,
   faPhoneAlt,
   faMapMarkerAlt,
+  faAngleRight,
 } from '@fortawesome/free-solid-svg-icons'
 
 import Customer from '../../models/Customer'
@@ -13,6 +14,7 @@ import Appointment, { AppointmentFragments } from '../../models/Appointment'
 import { AppointmentTypeFragments } from '../../models/AppointmentType'
 import { useQuery } from '@apollo/react-hooks'
 import LoadingOverlay from '../shared/LoadingOverlay'
+import AppointmentModal from './AppointmentModal'
 
 const AppointmentsQuery = gql`
   query AppointmentsQuery($customerId: Int!) {
@@ -40,6 +42,10 @@ const CustomerModal: React.FunctionComponent<Props> = ({
 }) => {
   if (!customer) return <div />
 
+  const [selectedAppointment, setSelectedAppointment] = React.useState<
+    Appointment
+  >()
+
   const appointmentsQueryResult = useQuery(AppointmentsQuery, {
     variables: { customerId: customer.id },
   })
@@ -64,56 +70,76 @@ const CustomerModal: React.FunctionComponent<Props> = ({
       <div className="fixed inset-0 bg-gray-700 opacity-25" />
 
       {/* Modal */}
-      <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="my-3 bg-white rounded-lg shadow-lg">
-          <div className="flex items-center justify-between px-5 py-4 border-b border-gray-200">
-            <h4 className="text-xl font-semibold">
-              {customer.firstName} {customer.lastName}
-            </h4>
-            <div>
-              <button onClick={onClose}>
-                <FontAwesomeIcon
-                  icon={faTimesCircle}
-                  className="text-red-500 hover:text-red-600"
-                />
-              </button>
+      {!selectedAppointment && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="my-3 bg-white rounded-lg shadow-lg">
+            <div className="flex items-center justify-between px-5 py-4 border-b border-gray-200">
+              <h4 className="text-xl font-semibold">
+                {customer.firstName} {customer.lastName}
+              </h4>
+              <div>
+                <button onClick={onClose}>
+                  <FontAwesomeIcon
+                    icon={faTimesCircle}
+                    className="text-red-500 hover:text-red-600"
+                  />
+                </button>
+              </div>
             </div>
-          </div>
 
-          <div className="px-8 py-2">
-            <h3 className="mb-4 text-4xl font-semibold leading-normal text-gray-800"></h3>
-            <div className="my-4 text-gray-700">
-              <label className="block mb-2">E-mail</label>
-              <FontAwesomeIcon icon={faEnvelope} className="mr-4" />
-              {customer.email}
-            </div>
-            <div className="my-4 text-gray-700">
-              <label className="block mb-2">Téléphone</label>
-              <FontAwesomeIcon icon={faPhoneAlt} className="mr-4" />
-              {customer.phone}
-            </div>
-            <div className="my-4 text-gray-700">
-              <label className="block mb-2">Adresse</label>
-              <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-4" />
-              {customer.address}
-            </div>
-          </div>
+            <div className="px-8 py-2">
+              <h3 className="mb-4 text-4xl font-semibold leading-normal text-gray-800"></h3>
 
-          {appointments.map((appointment, i) => (
-            <div key={i}>
-              <div className="flex flex-row items-center break-words cursor-pointer">
-                <p className="flex-1 hidden text-center md:block">
-                  {appointment.appointmentType?.name}
-                </p>
+              <div className="my-4 text-gray-700">
+                <label className="block mb-2">E-mail</label>
+                <FontAwesomeIcon icon={faEnvelope} className="mr-4" />
+                {customer.email}
               </div>
 
-              {i !== appointments.length - 1 && (
-                <hr className="my-4 border-b-1" />
-              )}
+              <div className="my-4 text-gray-700">
+                <label className="block mb-2">Téléphone</label>
+                <FontAwesomeIcon icon={faPhoneAlt} className="mr-4" />
+                {customer.phone}
+              </div>
+
+              <div className="my-4 text-gray-700">
+                <label className="block mb-2">Adresse</label>
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-4" />
+                {customer.address}
+              </div>
+
+              <div className="my-4 text-gray-700">
+                <label className="block mb-2">Rendez-vous</label>
+                {appointments.map((appointment, i) => (
+                  <div key={i}>
+                    <div
+                      className="flex flex-row items-center pl-2 break-words cursor-pointer"
+                      onClick={() => setSelectedAppointment(appointment)}
+                    >
+                      <FontAwesomeIcon icon={faAngleRight} className="mr-4" />
+                      <p>{appointment.appointmentType?.name}</p>
+                    </div>
+
+                    {i !== appointments.length - 1 && (
+                      <hr className="my-4 border-b-1" />
+                    )}
+                  </div>
+                ))}
+              </div>
             </div>
-          ))}
+          </div>
         </div>
-      </div>
+      )}
+
+      <AppointmentModal
+        appointment={
+          selectedAppointment
+            ? { ...selectedAppointment, customer: customer }
+            : undefined
+        }
+        onClose={() => setSelectedAppointment(undefined)}
+        useBackButton={true}
+      />
     </>
   )
 }
