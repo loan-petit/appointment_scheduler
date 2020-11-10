65fd31aa3ca96eb04605bb48b62b482d01e6c66e
loan-petit <petit.loan1@gmail.com>
2020-11-10 09:50:31 +0200

    fix: Prisma API postinstall shouldn't be started automatically by Dockerfile

--- prisma/src/server.ts
+++ prisma/src/server.ts
@@ -1,6 +1,5 @@
-import 'dotenv/config'
-
 import { GraphQLServer } from 'graphql-yoga'
+
 import { permissions } from './permissions'
 import { schema } from './schema'
 import { createContext } from './context'
@@ -10,6 +9,8 @@ new GraphQLServer({
   context: createContext,
   middlewares: [permissions],
 }).start(
-  { playground: process.env.NODE_ENV === 'production' ? false : '/' },
+  {
+    playground: process.env.NODE_ENV === 'production' ? false : '/',
+  },
   () => console.log('🚀 Server ready at: http://localhost:4000'),
 )
